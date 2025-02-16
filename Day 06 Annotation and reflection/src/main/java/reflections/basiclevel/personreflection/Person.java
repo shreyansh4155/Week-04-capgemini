145f54d3b1adf6cc25d525bb5a4c9e233dddd705
unknown <shreyanshg245@gmail.com>
2025-02-17 00:21:45 +0530

    personel reflection program added to the branch by shreyansh

---
+++ Day 06 Annotation and reflection/src/main/java/reflections/basiclevel/personreflection/Person.java
@@ -0,0 +1,13 @@
+package reflections.basiclevel.personreflection;
+
+public class Person {
+    private int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
