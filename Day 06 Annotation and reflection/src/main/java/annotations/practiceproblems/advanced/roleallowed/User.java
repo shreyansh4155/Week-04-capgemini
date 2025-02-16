4edc6825e939dc20a15ec0f2518155147606fc46
unknown <shreyanshg245@gmail.com>
2025-02-16 23:32:41 +0530

    role allowed program added to the day-06 branch by shreyansh

---
+++ Day 06 Annotation and reflection/src/main/java/annotations/practiceproblems/advanced/roleallowed/User.java
@@ -0,0 +1,13 @@
+package annotations.practiceproblems.advanced.roleallowed;
+
+class User {
+    private String role;
+
+    public User(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
